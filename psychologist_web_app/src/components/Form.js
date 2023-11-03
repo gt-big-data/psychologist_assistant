fe881b68bf1e95f0f909e2b6f1799fb877d8fa2d
tnittala <tnittala@gmail.com>
2023-11-03 11:45:57 -0400

    form component

---
+++ psychologist_web_app/src/components/Form.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <form>
+            <label>Enter your name: 
+                <input type="text"/>
+            </label>
+            <br/>
+            <label>Enter your age: 
+                <input type="text"/>
+            </label>
+            <br/>
+            <label>Enter your Ethnicity: 
+                <input type="text"/>
+            </label>
+            <br/>
+            <label>Veteran Status: 
+                <select name = "vetStatus">
+                    <option value = "active">Active</option>
+                    <option value = "retired">Retired</option>
+                    <option value = "notVet">Not a Veteran</option>
+                </select>
+            </label>
+            <br/>
+            <label>Please describe your history with substance use:
+                <input type="text"/>
+            </label>
+            <br/>
+            <label>Education: 
+                <select name = "edu">
+                <option value = "lowerLever">Less than High School</option>
+                    <option value = "highSchool">High School</option>
+                    <option value = "someCollege">Some College</option>
+                    <option value = "bachelors">Bachelors Degree</option>
+                    <option value = "masters">Masters Degree</option>
+                    <option value = "higherLevel">Above Masters Degree</option>
+                </select>
+            </label>
+        </form>
+    )
+}
+
+export default Form
