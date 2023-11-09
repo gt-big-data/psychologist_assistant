d05509c2ccb78b5f6619b5c21a23cb0d03059847
John Pham <johnpham456@gmail.com>
2023-11-09 17:46:59 -0500

    Capture form inputs in variables

--- psychologist_server/server.py
+++ psychologist_server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/test")
 def test():
     return {"test": ["test1","test2","test3"]}
+
+@app.route("/submit", methods=["POST"]) 
+def formSubmit():
+    form_data = request.get_json()
+    print(form_data)
+    return 'Done', 201
+
+
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- psychologist_web_app/src/pages/Form.js
+++ psychologist_web_app/src/pages/Form.js
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Form() {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [ethinicity, setEthnicity] = useState("");
+    const [vetStatus, setVetStatus] = useState("");
+    const [history, setHistory] = useState("");
+    const [education, setEducation] = useState("");
+  
     return (
         <form>
             <label>Enter your name: 
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value = {name}
+                    onChange = {e => setName(e.target.value)}
+                />
             </label>
             <br/>
             <label>Enter your age: 
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value = {age}
+                    onChange = {e => setAge(e.target.value)}
+                    />
             </label>
             <br/>
             <label>Enter your Ethnicity: 
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value = {ethinicity}
+                    onChange = {e => setEthnicity(e.target.value)}
+                />
             </label>
             <br/>
             <label>Veteran Status: 
-                <select name = "vetStatus">
+                <select 
+                    name = "vetStatus"
+                    value = {vetStatus}
+                    onChange = {e => setVetStatus(e.target.value)}
+                >
                     <option value = "active">Active</option>
                     <option value = "retired">Retired</option>
                     <option value = "notVet">Not a Veteran</option>
@@ -24,11 +47,19 @@ function Form() {
             </label>
             <br/>
             <label>Please describe your history with substance use:
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value = {history}
+                    onChange = {e => setHistory(e.target.value)}
+                />
             </label>
             <br/>
             <label>Education: 
-                <select name = "edu">
+                <select 
+                    name = "edu"
+                    value = {education}
+                    onChange = {e => setEducation(e.target.value)}
+                >
                 <option value = "lowerLever">Less than High School</option>
                     <option value = "highSchool">High School</option>
                     <option value = "someCollege">Some College</option>
@@ -38,7 +69,24 @@ function Form() {
                 </select>
             </label>
             <br/>
-            <input type="submit" value="Submit"></input>
+            <input 
+                type="submit" 
+                value="Submit"
+                onSubmit={async () => {
+                    const form_data = {name, age, ethinicity, vetStatus, history, education}
+                    const response = await fetch("http://localhost:8000/submit", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(form_data)
+                    });
+
+                    if (response.ok) {
+                        console.log("response worked!");
+                    }
+                }}
+            ></input>
         </form>
     )
 }
