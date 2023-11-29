429f5ee2563e65a4d9d24eec413c33872fbc8630
John Pham <johnpham456@gmail.com>
2023-11-28 21:01:29 -0500

    Send form data to backend then back to frontend

--- psychologist_server/server.py
+++ psychologist_server/server.py
@@ -1,6 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# Dummy data to simulate server processing
+processed_data = []
 
 # Routes
 @app.route("/test")
@@ -9,11 +14,20 @@ def test():
 
 @app.route("/submit", methods=["POST"]) 
 def formSubmit():
+    global processed_data
     form_data = request.get_json()
-    print(form_data)
-    return 'Done', 201
+    print("Received form data:", form_data)
 
+    # Process the form data 
+    # input form data -> process (run through ML model) -> processed data (output of ML model)
+    # Right now there is no processing, just returning the data
+    processed_data = form_data
 
+    return jsonify(processed_data), 201
+
+@app.route("/processed_data")
+def getProcessedData():
+    return jsonify(processed_data)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- psychologist_web_app/src/pages/Form.js
+++ psychologist_web_app/src/pages/Form.js
@@ -1,16 +1,64 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Form() {
     const [name, setName] = useState("");
-    const [age, setAge] = useState(0);
-    const [ethinicity, setEthnicity] = useState("");
+    const [age, setAge] = useState("");
+    const [ethnicity, setEthnicity] = useState("");
     const [vetStatus, setVetStatus] = useState("");
     const [history, setHistory] = useState("");
     const [education, setEducation] = useState("");
-  
+
+    const [submitStatus, setSubmitStatus] = useState({ status: '', message: '' });
+    const[data, setData] = useState([]);
+    
+    const formFieldsOrder = ["name", "age", "ethnicity", "vetStatus", "history", "education"];
+
+
+    const fetchData = async () => {
+    try {
+        const response = await fetch('http://localhost:8000/processed_data'); 
+        const result = await response.json();
+        console.log('Fetched data:', result);
+        setData(result);  
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+    };
+
+    const handleSubmit = async (e) => {
+        // When a form is submitted, the browser typically performs a default action, which is to send a request to the server and reload the page.
+        // This line prevents that.
+        e.preventDefault();
+    
+        const form_data = { name, age, ethnicity, vetStatus, history, education };
+    
+        // Sends a post request to /submit
+        try {
+          const response = await fetch('http://localhost:8000/submit', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form_data),
+          });
+    
+          if (response.ok) {
+            setSubmitStatus({ status: 'success', message: 'Form submitted successfully!' });
+            fetchData();
+          } else {
+            setSubmitStatus({ status: 'error', message: 'Error submitting form.' });
+          }
+        } catch (error) {
+          console.error('An unexpected error occurred:', error);
+          setSubmitStatus({ status: 'error', message: 'Unexpected error occurred.' });
+        }
+      };
+    
     return (
-        <form>
-            <label>Enter your name: 
+    <div>
+        {/* When the form is submitted, handleSubmit is ran. */}
+        <form onSubmit={handleSubmit}>
+        <label>Enter your name: 
                 <input 
                     type="text"
                     value = {name}
@@ -29,7 +77,7 @@ function Form() {
             <label>Enter your Ethnicity: 
                 <input 
                     type="text"
-                    value = {ethinicity}
+                    value = {ethnicity}
                     onChange = {e => setEthnicity(e.target.value)}
                 />
             </label>
@@ -69,26 +117,27 @@ function Form() {
                 </select>
             </label>
             <br/>
-            <input 
-                type="submit" 
-                value="Submit"
-                onSubmit={async () => {
-                    const form_data = {name, age, ethinicity, vetStatus, history, education}
-                    const response = await fetch("http://localhost:8000/submit", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify(form_data)
-                    });
-
-                    if (response.ok) {
-                        console.log("response worked!");
-                    }
-                }}
-            ></input>
+        <input type="submit" value="Submit" />
+        {submitStatus.status && (
+            <div>
+            <p>{submitStatus.message}</p>
+            </div>
+        )}
         </form>
-    )
-}
 
-export default Form
+
+        {submitStatus.status === 'success' && (
+        <div>
+          <h2>Results:</h2>
+          {formFieldsOrder.map((field) => (
+            <div key={field}>
+              <strong>{field.charAt(0).toUpperCase() + field.slice(1)}:</strong> {data[field]}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+    
+    export default Form;
